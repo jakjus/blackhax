aa29c2af16bd9c1bab6cc5cad6d2d9bcfecebf2e
idlehere <fazzebo@gmail.com>
2020-11-01 21:05:43 +0100

    Updates

---
+++ precache-manifest.82a7759ac940c3a78eebb574504a7eab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95543e085a97a5c855ebb45dbfd4f6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed59c224dc73605c8d03",
+    "url": "/static/css/main.a512f8fb.chunk.css"
+  },
+  {
+    "revision": "ab08552e3316acfb2f83",
+    "url": "/static/js/2.0b13c622.chunk.js"
+  },
+  {
+    "revision": "9b707ee842af4f317f626a3c3900dd73",
+    "url": "/static/js/2.0b13c622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed59c224dc73605c8d03",
+    "url": "/static/js/main.9f68d698.chunk.js"
+  },
+  {
+    "revision": "ccdd39d9078e004da1155a3245eb9166",
+    "url": "/static/js/main.9f68d698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc21b0cf950199eb1b0",
+    "url": "/static/js/runtime-main.a52b60f3.js"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
