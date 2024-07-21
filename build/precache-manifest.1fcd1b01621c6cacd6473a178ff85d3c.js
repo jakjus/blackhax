08898d48b5ad54cbed8b27ee6f04327c4cd6c041
jakjus <fazzebo@gmail.com>
2024-07-21 10:50:13 +0200

    build

---
+++ build/precache-manifest.1fcd1b01621c6cacd6473a178ff85d3c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c331948b427a61d214042ee0e3bbb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8600eb8a9b3ef6c9d2f8",
+    "url": "/static/css/main.26467d0c.chunk.css"
+  },
+  {
+    "revision": "b6bdaafb79bdeb27f309",
+    "url": "/static/js/2.5c7dd8c7.chunk.js"
+  },
+  {
+    "revision": "5f2ac350033ab5c4bebfa6b5635c1ace",
+    "url": "/static/js/2.5c7dd8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8600eb8a9b3ef6c9d2f8",
+    "url": "/static/js/main.ca3474e7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ca3474e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc21b0cf950199eb1b0",
+    "url": "/static/js/runtime-main.a52b60f3.js"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
